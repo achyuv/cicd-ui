c6d9cf9180c32ea4d32f67d5fce192f25b257725
Achyut Velpula <achyutvelpula@achyuts-mbp.lan>
2022-03-25 16:57:06 -0400

    bootstrap file added  - code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import {Planets} from './Components/Planets'
+import {Planets} from './Components/Planets';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
